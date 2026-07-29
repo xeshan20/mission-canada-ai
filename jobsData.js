1944b71718d153bfce50bfeadc127c6ca246b9f0
xeshan20 <zeeshanfgroup@yahoo.com>
2026-07-30 03:58:07 +0500

    Add initial jobs data for various positions

---
+++ jobsData.js
@@ -0,0 +1,43 @@
+const jobsData = [
+
+{
+title:"Driver",
+location:"Alberta",
+lmia:"Available",
+salary:"CAD $25 - $35/hour",
+match:90
+},
+
+{
+title:"Warehouse Worker",
+location:"Ontario",
+lmia:"Available",
+salary:"CAD $22 - $30/hour",
+match:88
+},
+
+{
+title:"Production Worker",
+location:"Quebec",
+lmia:"Requested",
+salary:"CAD $23 - $32/hour",
+match:85
+},
+
+{
+title:"Warehouse Supervisor",
+location:"British Columbia",
+lmia:"Available",
+salary:"CAD $30 - $40/hour",
+match:92
+},
+
+{
+title:"General Labourer",
+location:"Prince Edward Island",
+lmia:"Available",
+salary:"CAD $22 - $30/hour",
+match:84
+}
+
+];
